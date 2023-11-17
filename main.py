8c27f2f672e5a4f9f2b5c8ee7e09f3875dd23ad0
mariangel5 <mariangel.avendano.ramirez@est.una.ac.cr>
2023-11-17 00:25:48 -0600

    tengo errores que no son errores

--- main.py
+++ main.py
@@ -59,7 +59,6 @@ def split_parenthesis(linea):
     return tokens
 
 def guardarEnTablaSimbolos(file_path):
-
     tablaSimbolos = TablaSimbolos()
 
     with open(file_path, 'r') as file:
@@ -77,11 +76,13 @@ def guardarEnTablaSimbolos(file_path):
                     function_name = tokens[1]
                     return_type = tokens[0]
 
-                    tablaSimbolos.insertar_funcion(function_name, return_type)
+                    if tablaSimbolos.lookup_function(function_name):
+                        print(f"Error - Línea {line_num}: Función '{function_name}' redefinida.")
+                    else:
+                        tablaSimbolos.insertar_funcion(function_name, return_type)
 
                     # Guardar parametros
-
-                    i = 4  # Primer parametro despues de (
+                    i = 4  # Primer parametro después de (
                     while i < len(tokens) and tokens[i] != '{':
                         if tokens[i] == ',':
                             tablaSimbolos.insertar_variable(tokens[i - 1], tokens[i - 2])
@@ -92,10 +93,10 @@ def guardarEnTablaSimbolos(file_path):
                 else:
                     # Declarar variable
                     tablaSimbolos.insertar_variable(tokens[1], tokens[0])
+
     return tablaSimbolos
 
-def analyze_code(file_path):
-    symbol_table = TablaSimbolos()
+def analyze_code(file_path, symbol_table):
     errors = []
 
     with open(file_path, 'r') as file:
@@ -110,14 +111,22 @@ def analyze_code(file_path):
             keyword = tokens[0]
             last = tokens[-1]
 
-            # comprueba declaraciones de variables solas
-            if last != '{':
-                if keyword == 'int' or keyword == 'float' or keyword == 'string':
-                        symbol_table.insertar_variable(tokens[1], tokens[0])
-                else:
-                    print(f"Error - Línea {line_num}: Declaración de variable '{tokens[1]}' invalida.")
+            # Check if it's a variable declaration or assignment
+            if '=' in tokens:
+                variable_name = tokens[tokens.index('=') - 1]
+                variable_type = symbol_table.buscar(variable_name)
 
-            # comprueba declaraciones de funciones
+                if variable_type is None:
+                    errors.append(f"Error - Línea {line_num}: Uso de variable no declarada '{variable_name}'.")
+
+            # Check declaraciones de variables
+            elif last != '{':
+                if keyword == 'int' or keyword == 'float' or keyword == 'string':
+                    symbol_table.insertar_variable(tokens[1], tokens[0])
+                else:
+                    errors.append(f"Error - Línea {line_num}: Declaración de variable '{tokens[1]}' invalida.")
+
+            # Check declaraciones de funciones
             if last == '{':
                 if keyword == 'int' or keyword == 'float' or keyword == 'string' or keyword == 'void':
                     # Function declaration
@@ -133,15 +142,25 @@ def analyze_code(file_path):
                 # End of function scope
                 current_function = None
 
-            # revisa que el retorno sea correcto
+            # Revisar que el retorno sea correcto
             elif current_function:
                 # Inside a function
                 if keyword == 'return':
-                    # Check return type
+                    # Check if the function has a return type
                     expected_return_type = symbol_table.lookup_function(current_function)
-                    if expected_return_type and len(tokens) >= 2 and tokens[1] != expected_return_type:
+                    if not expected_return_type:
                         errors.append(
-                            f"Error - Línea {line_num}: Tipo de retorno incorrecto para la función '{current_function}'.")
+                            f"Error - Línea {line_num}: La función '{current_function}' no tiene tipo de retorno declarado.")
+                    else:
+                        # Check if the return type matches the expected return type
+                        if len(tokens) >= 2:
+                            returned_value_type = symbol_table.buscar(tokens[1])
+                            if returned_value_type != expected_return_type:
+                                errors.append(
+                                    f"Error - Línea {line_num}: Tipo de retorno incorrecto para la función '{current_function}'.")
+                        else:
+                            errors.append(
+                                f"Error - Línea {line_num}: Falta valor de retorno para la función '{current_function}'.")
 
                 elif keyword == 'if' or keyword == 'while':
                     # Add your code to handle if/while statements inside functions
@@ -163,5 +182,6 @@ def analyze_code(file_path):
 
 
 # Ejemplo de uso
-file_path = '../codigo_fuente.txt'
-print(guardarEnTablaSimbolos(file_path))
+file_path = 'codigo_fuente.txt'
+symbol_table = guardarEnTablaSimbolos(file_path)
+analyze_code(file_path, symbol_table)
